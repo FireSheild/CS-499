5f2fada19e4501e4ac5725fdc9005c64ba187a32
FireSheild <128265477+FireSheild@users.noreply.github.com>
2026-04-26 20:30:20 -0400

    Add files via upload

---
+++ v4/service/AnimalService.java
@@ -0,0 +1,14 @@
+package service;
+import repository.AnimalRepository;
+import model.RescueAnimal;
+public class AnimalService{
+    private final AnimalRepository repo;
+    public AnimalService(AnimalRepository repo){this.repo=repo;}
+    public void add(RescueAnimal a){repo.save(a);}
+    public void reserve(String id){
+        repo.find(id).ifPresent(a->{if(!a.isReserved()){a.reserve();repo.save(a);}});
+    }
+    public void printAll(){
+        repo.findAll().forEach(a->System.out.println(a.getId()+" | "+a.getName()+" | reserved="+a.isReserved()));
+    }
+}
